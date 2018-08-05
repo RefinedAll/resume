98e8d96d3ff07f4c6f78bb578088229d1fd3594c
zsail <zhangsiao1314@126.com>
2018-08-05 23:38:29 +0800

    修改手机PC判断

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import Vue from 'vue'
 import App from './App'
 import Mobile from './Mobile'
 
-var width = document.documentElement.clientWidth
+//var width = document.documentElement.clientWidth
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  render: h => h(width > 500 ? App : Mobile)
+  /*根据宽度判断 */
+  //render: h => h(width > 500 ? App : Mobile)
+  render: h => h((navigator.userAgent
+    .match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)) ?
+    Mobile : App)
 })
