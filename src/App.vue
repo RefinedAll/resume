c3a4e79d854b0efa5b9417d6c82e04a29df78d41
Administrator <zhangsiao1314@126.com>
2018-08-16 16:32:01 +0800

    resume

--- src/App.vue
+++ src/App.vue
@@ -10,9 +10,9 @@
 import StyleEditor from "./components/StyleEditor";
 import ResumeEditor from "./components/ResumeEditor";
 import "./assets/reset.css";
-import Stars from '../static/js/Stars';
-import Moon from '../static/js/Moon';
-import Meteor from '../static/js/Meteor';
+import Stars from "../static/js/Stars";
+import Moon from "../static/js/Moon";
+import Meteor from "../static/js/Meteor";
 
 export default {
   name: "app",
@@ -107,57 +107,54 @@ export default {
     }
   },
   mounted() {
-		let canvas = document.getElementById('canvas'),
-    ctx = canvas.getContext('2d'),
-    width = window.innerWidth,
-    height = window.innerHeight,
-    //实例化月亮和星星。流星是随机时间生成，所以只初始化数组
-    moon = new Moon(ctx, width, height),
-    stars = new Stars(ctx, width, height, 200),
-    meteors = [],
-    count = 0
+    let canvas = document.getElementById("canvas"),
+      ctx = canvas.getContext("2d"),
+      width = window.innerWidth,
+      height = window.innerHeight,
+      //实例化月亮和星星。流星是随机时间生成，所以只初始化数组
+      moon = new Moon(ctx, width, height),
+      stars = new Stars(ctx, width, height, 200),
+      meteors = [],
+      count = 0;
 
-canvas.width = width
-canvas.height = height
+    canvas.width = width;
+    canvas.height = height;
 
-//流星生成函数
-const meteorGenerator = ()=> {
-    //x位置偏移，以免经过月亮
-    let x = Math.random() * width + 800
-    meteors.push(new Meteor(ctx, x, height))
+    //流星生成函数
+    const meteorGenerator = () => {
+      //x位置偏移，以免经过月亮
+      let x = Math.random() * width + 800;
+      meteors.push(new Meteor(ctx, x, height));
 
-    //每隔随机时间，生成新流星
-    setTimeout(()=> {
-        meteorGenerator()
-    }, Math.random() * 2000)
-}
+      //每隔随机时间，生成新流星
+      setTimeout(() => {
+        meteorGenerator();
+      }, Math.random() * 2000);
+    };
 
-//每一帧动画生成函数
-const frame = ()=> {
-    //每隔10帧星星闪烁一次，节省计算资源
-    count++
-    count % 10 == 0 && stars.blink()
+    //每一帧动画生成函数
+    const frame = () => {
+      //每隔10帧星星闪烁一次，节省计算资源
+      count++;
+      count % 10 == 0 && stars.blink();
 
-    moon.draw()
-    stars.draw()
+      moon.draw();
+      stars.draw();
 
-    meteors.forEach((meteor, index, arr)=> {
+      meteors.forEach((meteor, index, arr) => {
         //如果流星离开视野之内，销毁流星实例，回收内存
         if (meteor.flow()) {
-            meteor.draw()
+          meteor.draw();
         } else {
-            arr.splice(index, 1)
+          arr.splice(index, 1);
         }
-    })
-    requestAnimationFrame(frame)
-}
+      });
+      requestAnimationFrame(frame);
+    };
 
-meteorGenerator()
-frame()
-	},
-
-
-
+    meteorGenerator();
+    frame();
+  }
 };
 
 let fullStyle_0 = `
@@ -276,6 +273,10 @@ java后台研发工程师。
 ----
 1.北京安宏睿业科技有限公司
 
+自我评价
+----
+本人静若处子动如脱兔，责任心强从不中途跑路
+
 友情链接
 ----
 
@@ -299,7 +300,7 @@ html {
   box-sizing: border-box;
 }
 .canvas {
-    position: fixed;
-    z-index: -1;
+  position: fixed;
+  z-index: -1;
 }
 </style>
